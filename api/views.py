d1f313c392da9c47a1118d751c76171f98971663
dav1ta <daviti.tchanturia.1@btu.edu.ge>
2023-06-16 01:05:31 +0400

    test

--- api/views.py
+++ api/views.py
@@ -1,44 +1,14 @@
-from rest_framework.response import Response
-from rest_framework import viewsets
-from api.models import Incident,Description,Cve_number,Target
-from .serializers import IncidentSerializer,TargetSerializer,DescriptionSerializer,CVESerializer
-from datetime import datetime
 
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+import random
 
+@api_view(['GET'])
+def my_view(request):
+    if request.method == 'GET':
+        data = {
+            "key1": random.randint(1, 1000),
+            "key2": "value2"
+        }
+        return JsonResponse(data)
 
-
-class IncidentViewset(viewsets.ModelViewSet):
-    queryset = Incident.objects.all()
-    serializer_class = IncidentSerializer
-
-    def get_queryset(self):
-        start_date = self.request.query_params.get('start_date')
-        end_date = self.request.query_params.get('end_date')
-        cve = self.request.query_params.get('cve')
-        queryset = self.queryset
-        if start_date:
-             queryset = queryset.filter(time_creation__gte=datetime.strptime(start_date,"%Y-%m-%d %H:%M:%S"))
-        if end_date:
-             queryset = queryset.filter(time_creation__lte=datetime.strptime(end_date,"%Y-%m-%d %H:%M:%S"))
-        
-        if end_date:
-             queryset = queryset.filter(time_creation__lte=datetime.strptime(end_date,"%Y-%m-%d %H:%M:%S"))
-
-        if cve:
-            queryset = queryset.filter(cve_number__cve=cve)
-        return queryset
-
-class TargeViewset(viewsets.ModelViewSet):
-    queryset = Target.objects.all()
-    serializer_class = TargetSerializer
-
-
-
-class DescriptionViewset(viewsets.ModelViewSet):
-    queryset = Description.objects.all()
-    serializer_class = DescriptionSerializer
-
-
-class CveViewset(viewsets.ModelViewSet):
-    queryset = Cve_number.objects.all()
-    serializer_class = CVESerializer
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from api.views import my_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('test/', my_view, name='my_view'),
 ]
